be2052d9564405452ae69ab02780a6e7f4b04d86
Andres Arevalo <andresfelipear@gmail.com>
2022-01-05 13:11:12 -0800

    exercise 2.f done

--- app.js
+++ app.js
@@ -8,10 +8,27 @@ let myArr = [2,3,5,5]
 
 //mySecondArr
 let mySecondArr = myArr.map(num => num*2);
+
+//Average mySecondArr
+const avrgMySecond = mySecondArr.reduce((a,b)=> a+b)/mySecondArr.length
+
 let result =  sum(...myArr)
 let result2 = sum(...mySecondArr)
+
+//function filter
+const fFilter = (arr)=>{
+    let result = arr.filter((num)=>num>avrgMySecond)
+    return result
+}
 
 console.log("Exercise 1");
 console.log("myArr: " +result);
 console.log("mySecondArr: " +result2);
+console.log("average: "+ avrgMySecond);
+console.log("filter: "+ fFilter(mySecondArr));
+
+
+
+
+
 
