db8bd28b9c9794975650d3208b9cd475d5f37edf
Andres Arevalo <andresfelipear@gmail.com>
2022-01-05 13:03:49 -0800

    exercise 2.e done

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const sum = require('./fsum')
 //myArr
 let myArr = [2,3,5,5]
 
+//mySecondArr
+let mySecondArr = myArr.map(num => num*2);
 let result =  sum(...myArr)
+let result2 = sum(...mySecondArr)
+
 console.log("Exercise 1");
-console.log(result);
+console.log("myArr: " +result);
+console.log("mySecondArr: " +result2);
+
